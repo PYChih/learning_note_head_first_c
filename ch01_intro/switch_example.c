5298821bf86d6fa5c6176ed81fde814c9182b207
PYChih <poyuanchih@gapp.nthu.edu.tw>
2021-04-29 16:31:48 +0800

    ch1 intro

--- ch01_intro/switch_example.c
+++ ch01_intro/switch_example.c
@@ -11,22 +11,22 @@ int main() {
     scanf("%2s", card_name);
     int val = 0;
     switch (card_name[0]) {
-    case 'K':
-    case 'Q':
-    case 'J':
-      val = 10;
-      break;
-    case 'A':
-      val = 11;
-      break;
-    case 'X':
-      continue;
-    default:
-      val = atoi(card_name);
-      if ((val <= 1) || (val > 10)) {
-        puts("I don't understand that value!");
+      case 'K':
+      case 'Q':
+      case 'J':
+        val = 10;
+        break;
+      case 'A':
+        val = 11;
+        break;
+      case 'X':
         continue;
-      }
+      default:
+        val = atoi(card_name);
+        if ((val <= 1) || (val > 10)) {
+          puts("I don't understand that value!");
+          continue;
+        }
     }
     if ((val > 2) && (val < 7)) {
       count++;
